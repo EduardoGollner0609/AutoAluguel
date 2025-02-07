6b41c58ccc3b1070de43f7c802761d7d26b31684
EduGollner0609 <dudugollner@gmail.com>
2025-02-07 05:44:05 -0300

    Criando projeto autoaluguel.

---
+++ src/main/java/com/eduardo/autoaluguel/AutoaluguelApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.autoaluguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoaluguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoaluguelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/autoaluguel/AutoaluguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.autoaluguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoaluguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
