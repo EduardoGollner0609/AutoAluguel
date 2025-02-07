7430085a33a836d72dcec53f776a6aceb0bbd943
EduGollner0609 <dudugollner@gmail.com>
2025-02-07 09:16:07 -0300

    Criando camada Controller da entidade Location.

---
+++ src/main/java/com/eduardo/autoaluguel/controllers/LocationController.java
@@ -0,0 +1,59 @@
+package com.eduardo.autoaluguel.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.eduardo.autoaluguel.dtos.LocationDTO;
+import com.eduardo.autoaluguel.services.LocationService;
+
+@RestController
+@RequestMapping(value = "/locations")
+public class LocationController {
+
+	@Autowired
+	private LocationService service;
+
+	@PostMapping
+	public ResponseEntity<LocationDTO> insert(@RequestBody LocationDTO locationDTO) {
+		locationDTO = service.insert(locationDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(locationDTO.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(locationDTO);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<LocationDTO>> findAll() {
+		List<LocationDTO> automobiles = service.findAll();
+		return ResponseEntity.ofNullable(automobiles);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<LocationDTO> findById(@PathVariable Long id) {
+		LocationDTO locationDTO = new LocationDTO();
+		return ResponseEntity.ok(locationDTO);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<LocationDTO> update(@PathVariable Long id, @RequestBody LocationDTO locationDTO) {
+		locationDTO = service.update(id, locationDTO);
+		return ResponseEntity.ok(locationDTO);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+}
