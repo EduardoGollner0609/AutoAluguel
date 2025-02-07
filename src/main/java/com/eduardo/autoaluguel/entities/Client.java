dffe3db19672caac3a2a012a85252fb95e454362
EduGollner0609 <dudugollner@gmail.com>
2025-02-07 05:52:35 -0300

    Anotações para mapeamento objeto relacional.

--- src/main/java/com/eduardo/autoaluguel/entities/Client.java
+++ src/main/java/com/eduardo/autoaluguel/entities/Client.java
@@ -1,10 +1,22 @@
 package com.eduardo.autoaluguel.entities;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
 public class Client {
 
-	private String nome;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
 	private String cpf;
 	private String email;
 	private String phone;
@@ -14,9 +26,10 @@ public class Client {
 	public Client() {
 	}
 
-	public Client(String nome, String cpf, String email, String phone, LocalDate birthdate, String address) {
+	public Client(Long id, String name, String cpf, String email, String phone, LocalDate birthdate, String address) {
 		super();
-		this.nome = nome;
+		this.id = id;
+		this.name = name;
 		this.cpf = cpf;
 		this.email = email;
 		this.phone = phone;
@@ -24,12 +37,20 @@ public class Client {
 		this.address = address;
 	}
 
-	public String getNome() {
-		return nome;
+	public Long getId() {
+		return id;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getCpf() {
@@ -70,6 +91,23 @@ public class Client {
 
 	public void setAddress(String address) {
 		this.address = address;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
