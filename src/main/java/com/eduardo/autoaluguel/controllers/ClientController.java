cf01d2e3de036ef336bccc3da2cfea5dc1b2f8c3
EduGollner0609 <dudugollner@gmail.com>
2025-02-07 07:36:13 -0300

    Criando camada Controller da entidade Client.

---
+++ src/main/java/com/eduardo/autoaluguel/controllers/ClientController.java
@@ -0,0 +1,38 @@
+package com.eduardo.autoaluguel.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.eduardo.autoaluguel.dtos.ClientDTO;
+import com.eduardo.autoaluguel.services.ClientService;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+	@Autowired
+	private ClientService service;
+
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO clientDTO) {
+		clientDTO = service.insert(clientDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(clientDTO.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(clientDTO);
+	}
+
+	@GetMapping(value = "/{cpf}")
+	public ResponseEntity<ClientDTO> findByCpf(@PathVariable String cpf) {
+		ClientDTO clientDTO = service.findByCpf(cpf);
+		return ResponseEntity.ok(clientDTO);
+	}
+}
