fa4791f941c667d2bd8cb73c5e1f4679ad1524dc
EduGollner0609 <dudugollner@gmail.com>
2025-02-07 21:53:52 -0300

    Estilizando Header.

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,11 +1,29 @@
+import { NavLink } from 'react-router-dom';
 import './styles.css';
 
 
 export default function Header() {
     return (
         <header>
-            <nav>
-                <h1>Auto Aluguel</h1>
+            <nav className="container">
+
+                <div className="header-title">
+                    <h1>AutoAluguel</h1>
+                </div>
+                <div className="header-options-navegation">
+                    <ul className="header-options-navegation-list">
+                        <li className="header-option-item">
+                            <NavLink to="/">
+                                Inicio
+                            </NavLink>
+                        </li>
+                        <li className="header-option-item">
+                            <NavLink to="/painel">
+                                Alugar
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
     );
