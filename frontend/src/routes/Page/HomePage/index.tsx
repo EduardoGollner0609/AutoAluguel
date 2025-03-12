8c647148bb21be1fd928e5accb0a41f39fb84513
EduGollner0609 <dudugollner@gmail.com>
2025-03-12 16:38:10 -0300

    git add .

--- frontend/src/routes/Page/HomePage/index.tsx
+++ frontend/src/routes/Page/HomePage/index.tsx
@@ -5,18 +5,24 @@ import { AutomobileDTO } from '../../../models/automobile';
 import * as automobileService from '../../../services/automobile-service';
 import { Link } from 'react-router-dom';
 import loadingIcon from '../../../assets/spinner-loading-icon.svg';
+import ButtonNextPage from '../../../components/ButtonNextPage';
 
 export default function HomePage() {
 
     const [automobiles, setAutomobiles] = useState<AutomobileDTO[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [automobilesIsEmpty, setAutomobileIsEmpty] = useState<boolean>(false);
+    const [numberPage, setNumberPage] = useState<number>(0);
+    const [isLastPage, setIsLastPage] = useState<boolean>(false);
+
 
     useEffect(() => {
         setLoading(true);
         automobileService.findAllOrderByReturned().then(
             response => {
-                setAutomobiles(response.data);
+                const nextPage = response.data.content;
+                setAutomobiles(automobiles.concat(nextPage))
+                setIsLastPage(response.data.last);
                 setLoading(false);
                 if (automobiles.length === 0) {
                     setAutomobileIsEmpty(true);
@@ -24,6 +30,10 @@ export default function HomePage() {
             }
         )
     }, []);
+
+    function handleNextPageClick() {
+        setNumberPage(numberPage + 1);
+    }
 
     return (
         <section id="home-page-section" className="container">
@@ -53,6 +63,9 @@ export default function HomePage() {
                 <div className="loading-catalog-list">
                     <img src={loadingIcon} alt="" />
                 </div>
+            }
+            {
+                (automobiles.length > 0 && !isLastPage && !loading) && <ButtonNextPage onNextPage={handleNextPageClick} />
             }
         </section >
     );
