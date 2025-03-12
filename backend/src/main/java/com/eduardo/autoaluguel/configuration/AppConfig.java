e9020e002a8f4c635052d5919c9f96d034f841eb
EduGollner0609 <dudugollner@gmail.com>
2025-03-12 16:11:53 -0300

    Passando corsOrigins em variavel de ambiente.

--- backend/src/main/java/com/eduardo/autoaluguel/configuration/AppConfig.java
+++ backend/src/main/java/com/eduardo/autoaluguel/configuration/AppConfig.java
@@ -1,14 +1,19 @@
 package com.eduardo.autoaluguel.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
+
+	@Value("${cors.origins}")
+	private String corsOrigins;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:5173")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*").allowCredentials(true);
+		registry.addMapping("/**").allowedOrigins(corsOrigins).allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("*").allowCredentials(true);
 	}
 }
